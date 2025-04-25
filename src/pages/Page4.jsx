49324ab1fa2153fec8a3137ef8b3743bbca90b75
Shriyam802 <cseshriyam802@gmail.com>
2025-04-25 11:59:51 +0530

    Update Page4.jsx

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+
+import videoSrc from '../assets/shr.mp4';
 
 function Page4() {
   return (
     <div className='h-screen p-3 mt-5 bg-white overflow-hidden '>
     <div className='h-full w-full text-white  bg- black relative '>
-           <video className=' ' autoplay="true"  muted loop  src="src/assets/shr.mp4"></video>
+           <video className=' ' autoplay="true"  muted loop >
+              <source src={videoSrc} type="video/mp4" />
+           </video>
         <div className=' absolute inset-4  border-13 border-white rounded-[35px] '>
              <h1 className='text-center text-[18vw] text-white-900 opacity-75  font-[anzo]'>THE</h1>
                     <h1 className='text-center text-[20vw] text-white font-[anzo] opacity-75 leading-2'>FOCUS</h1>
